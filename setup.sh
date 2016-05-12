2c4d2a096e723e6005f0556386bc0d90d9c6bc0d
Patrick Bassut <patrickbassut@hotmail.com>
2016-05-12 14:39:41 -0300

    Making setup script more complete

--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
+# symlink vim to ~/
 ln -s $PWD/.vim/ ~/.vim
+ln -s ~/.vim/vimrc ~/.vimrc
+ln -s ~/.vim/gvimrc ~/.gvimrc
+
+# symlink batch comamdns and env vars
 ln -s $PWD/.bash_profile ~/.bash_profile
 ln -s $PWD/.gitignore_global ~/.gitignore_global
 ln -s $PWD/Brewfile ~/Brewfile
 
-ln -s ~/.vim/vimrc ~/.vimrc
-ln -s ~/.vim/gvimrc ~/.gvimrc
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+vim +PluginInstall +qall
