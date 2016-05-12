1921f4e221c2cf3a4cb7f04f812ffec2ffe454ea
Patrick Bassut <patrickbassut@hotmail.com>
2016-05-11 22:44:21 -0300

    Using relative path

---
+++ setup.sh
@@ -0,0 +1,7 @@
+ln -s $PWD/.vim/ ~/.vim
+ln -s $PWD/.bash_profile ~/.bash_profile
+ln -s $PWD/.gitignore_global ~/.gitignore_global
+ln -s $PWD/Brewfile ~/Brewfile
+
+ln -s ~/.vim/vimrc ~/.vimrc
+ln -s ~/.vim/gvimrc ~/.gvimrc
