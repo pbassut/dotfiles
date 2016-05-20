1ed03e7b655da6026f313a23f5f2167ab6fabff7
Patrick Bassut <patrickbassut@hotmail.com>
2016-05-20 15:24:55 -0300

    Adding .gitconfig bindings

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ ln -s ~/.vim/gvimrc ~/.gvimrc
 # symlink batch comamdns and env vars
 ln -s $PWD/.bash_profile ~/.bash_profile
 ln -s $PWD/.gitignore_global ~/.gitignore_global
+ln -s $PWD/.gitconfig ~/.gitconfig
 ln -s $PWD/Brewfile ~/Brewfile
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
