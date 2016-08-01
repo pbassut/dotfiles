388cfa70a803707fa6ecf455d375aa6d579b7f87
Patrick Bassut <patrick.bassut@esharesinc.com>
2016-08-01 09:54:05 -0700

    Adding git completion

--- setup.sh
+++ setup.sh
@@ -5,8 +5,11 @@ ln -s ~/.vim/gvimrc ~/.gvimrc
 
 # symlink batch comamdns and env vars
 ln -s $PWD/.bash_profile ~/.bash_profile
+
 ln -s $PWD/.gitignore_global ~/.gitignore_global
 ln -s $PWD/.gitconfig ~/.gitconfig
+mv $PWD/.git-completion.bash ~/
+
 ln -s $PWD/Brewfile ~/Brewfile
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
